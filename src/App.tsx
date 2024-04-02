dbe02f978ed1207ed5a52ac40b5f4515dd643c50
ZeyXs <basile.gamer@gmail.com>
2024-04-02 18:20:40 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <p className="text-white text-3xl">Hello world!</p>
+    </div>
+  )
+}
+
+export default App
