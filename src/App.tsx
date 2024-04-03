74f68bd8ac2941bb11eb5157b41f59908ecee7af
ZeyXs <basile.gamer@gmail.com>
2024-04-03 10:42:11 +0200

    New push

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ function App() {
 
   return (
     <div className="h-screen bg-black flex items-center justify-center">
-      <p className="text-white text-3xl">Hello world!</p>
+      <p className="text-white text-3xl">Work in progress...</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
