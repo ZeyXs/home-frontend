5b80c0fb70093f98e896a795dc0d30f99b04adf5
ZeyXs <basile.gamer@gmail.com>
2024-04-08 15:17:37 +0200

    Added new content to staging

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
 
   return (
-    <div className="h-screen bg-black flex items-center justify-center">
+    <div className="h-screen bg-black flex flex-col space-y-2 items-center justify-center">
       <p className="text-white text-3xl">Et voilà, tout marche</p>
+      <p className="text-white text-lg">...même avec un basculement de branche !</p>
     </div>
   );
 };
