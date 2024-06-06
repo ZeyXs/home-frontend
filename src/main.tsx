a44f3682d8619b325eb455626b1698388ba4daeb
ZeyXs <basile.gamer@gmail.com>
2024-06-06 14:19:33 +0200

    Added Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+
 import './index.css'
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />
+    }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+    <RouterProvider router={router} />
+);
