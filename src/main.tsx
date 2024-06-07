ca8e74dddec9488af137cbf5bc990a4138f45589
ZeyXs <basile.gamer@gmail.com>
2024-06-07 10:21:11 +0200

    Added favicons and configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
+import HomePage from '@/pages/HomePage';
 
 import './index.css'
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
-const Home = () => {
+import { useEffect } from "react";
+
+const PAGE_NAME: string = "Home – ZeyX.dev"
+
+export default function HomePage() {
+
+  useEffect(() => {
+    document.title = PAGE_NAME;
+  }, []);
+
   return (
     <div>Home</div>
   );
 }
-
-export default Home;
